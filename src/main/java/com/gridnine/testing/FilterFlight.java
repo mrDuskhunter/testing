4044dd53a40acdf089e613f981ede16e7218bf61
mrDuskhunter <9162026242@list.ru>
2021-03-15 16:24:51 +0300

    v1.1.1 add return list for FilterFlight.filters add method FilterFlight.printFilterList add multiFiltration(2x) -> method overloading FilterFlight.printFilterList()

--- src/main/java/com/gridnine/testing/FilterFlight.java
+++ src/main/java/com/gridnine/testing/FilterFlight.java
@@ -19,8 +19,19 @@ public class FilterFlight {
          */
         }
     }
+    //if multiFiltration(2x) -> firstFilter(filterId) -> secondFilter(filterId2)
+    public static void printFilterList(int filterId, int filterId2, List<Flight> flightList) {
+        switch ((filterId*10)+filterId2) {
+            case 12 -> filter2(filter1(flightList)).forEach(System.out::println);
+            case 13 -> filter3(filter1(flightList)).forEach(System.out::println);
+            case 21 -> filter1(filter2(flightList)).forEach(System.out::println);
+            case 23 -> filter3(filter2(flightList)).forEach(System.out::println);
+            case 31 -> filter1(filter3(flightList)).forEach(System.out::println);
+            case 32 -> filter2(filter3(flightList)).forEach(System.out::println);
+        }
+    }
 
-    private static List<Flight> filter1(List<Flight> flightList) {
+    public static List<Flight> filter1(List<Flight> flightList) {
         //вылет до текущего момента времени
         List<Flight> list = new ArrayList<>();
         LocalDateTime timeNow = LocalDateTime.now();
@@ -36,7 +47,7 @@ public class FilterFlight {
         return list;
     }
 
-    private static List<Flight> filter2(List<Flight> flightList) {
+    public static List<Flight> filter2(List<Flight> flightList) {
         //имеются сегменты с датой прилёта раньше даты вылета
         List<Flight> list = new ArrayList<>();
 
@@ -51,7 +62,7 @@ public class FilterFlight {
         return list;
     }
 
-    private static List<Flight> filter3(List<Flight> flightList) {
+    public static List<Flight> filter3(List<Flight> flightList) {
         /*общее время, проведённое на земле превышает два часа (время на земле — это интервал между
                                                     прилётом одного сегмента и вылетом следующего за ним)
          */
